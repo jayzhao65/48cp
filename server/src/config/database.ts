173fa9d101b5f6647045985fa5b2352fb4799f32
jayzhao65 <158057002+jayzhao65@users.noreply.github.com>
2024-11-28 01:12:37 +0800

    1

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -4,14 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGODB_URI = process.env.NODE_ENV === 'production'
-  ? 'mongodb://8.218.98.220:27017/48cp?authSource=admin' // 确保用户名和密码正确
+  ? 'mongodb://8.218.98.220:27017/48cp' // 确保用户名和密码正确
   : 'mongodb://localhost:27017/48cp';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      authSource: 'admin'  // 添加这个选项
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
