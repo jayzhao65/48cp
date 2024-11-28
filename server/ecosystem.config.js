562dc1077d9c5cb2a444bbd86467e572ba279821
jayzhao65 <158057002+jayzhao65@users.noreply.github.com>
2024-11-28 22:20:36 +0800

    1

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [{
-    name: '48cp-backend',
-    script: 'dist/app.js',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
+    name: "48cp",
+    script: "./dist/app.js",
+    watch: true,
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "./logs/error.log",
+    out_file: "./logs/out.log",
+    log_file: "./logs/combined.log",
+    time: true
   }]
-};
+}
